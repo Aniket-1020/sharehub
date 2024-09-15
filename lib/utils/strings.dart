5bb5058c557acbb4d1bc5092e532e350c9c80ab3
aayushiss <aayushri1302@gmail.com>
2024-09-15 23:17:26 +0530

    added homepage_screen.dart

---
+++ lib/utils/strings.dart
@@ -0,0 +1,7 @@
+class AppStrings {
+  static const String appTitle = "ShareHub";
+  static const String scan = "Enter an Event";
+  static const String add = "Create an Event";
+  static const String profile = "My profile";
+
+}
